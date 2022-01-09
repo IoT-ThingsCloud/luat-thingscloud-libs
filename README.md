0ed80bd0d9c2af8c5944e51a89941d454b6614b4
ThingsCloud <github@qijiwanwu.com>
2022-01-09 22:13:54 +0800

    initial

---
+++ README.md
@@ -0,0 +1,63 @@
+这里是合宙模组 luat 方式接入 [ThingsCloud](https://www.thingscloud.xyz/) 云平台的 lib 库，以及示例代码。
+
+## 实现功能
+
+该代码库面向对合宙luat开发框架有一定的掌握的硬件开发者。通过封装良好的 lib 库，5分钟即可将模组接入 ThingsCloud 云平台，实现以下功能：
+
+- 上报模组端的数据，例如各种串口外设传感器的数据，可定时上报。
+- 模组可实时接收云平台下发的数据，包括属性下发或命令下发。
+- 支持自定义Topic，需结合云平台设备类型的自定义数据流。
+- 支持一机一密身份验证，也支持一型一密，便于相同设备类型下的所有设备使用相同固件。
+
+相关 URL：
+- [ThingsCloud MQTT 接入文档](https://docs.thingscloud.xyz/guide/connect-device/mqtt.html)
+- [ThingsCloud 控制台](https://www.thingscloud.xyz/)
+
+
+## 快速运行示例
+
+### testBasic
+
+使用 luatools 或 VSCode LuatIDE，将以下脚本文件烧录到模组。
+
+- `main.lua`
+- `testBasic.lua` 
+- `thingsCloud.lua`
+
+`thingsCloud.lua` 是 lib 库文件，无需修改。
+
+`testBasic.lua` 是示例程序，展示了基本用法，只需修改以下部分即可运行。
+
+```lua
+-- 以下复制设备证书和MQTT接入点地址，在设备详情页的【连接】页面可以找到。请勿泄露设备证书。
+-- ProjectKey
+local projectKey = ""
+-- AccessToken
+local accessToken = ""
+-- MQTT 接入点，只需主机名部分
+local host = "bj-3-mqtt.iot-api.com"
+```
+在 ThingsCloud 云平台创建设备后，在设备详情页的【连接】页面可以找到证书和MQTT接入点，如下：
+
+![image](https://user-images.githubusercontent.com/97299260/148683169-b5ef8f41-0960-4298-8269-2b792179e8f2.png)
+
+固件成功烧录后，模组即可成功接入平台，定时上报数据。
+
+![image](https://user-images.githubusercontent.com/97299260/148683283-a25871b7-b7b7-4e88-9767-e863240aaa2c.png)
+
+设备详情页显示设备属性实时数据。
+
+![image](https://user-images.githubusercontent.com/97299260/148683057-797cc9dd-f7cd-4948-9b43-f03d944c0555.png)
+
+可查看数据历史。
+
+![image](https://user-images.githubusercontent.com/97299260/148683088-ae05c067-7700-4429-bed2-a8aef656518c.png)
+
+下发属性
+
+![image](https://user-images.githubusercontent.com/97299260/148683107-c6b0aff6-d5b1-4424-98cf-c4d7bdf3695f.png)
+
+下发命令
+
+![image](https://user-images.githubusercontent.com/97299260/148683123-487cdfc8-9615-42c7-a5b6-b18e936b94fc.png)
+
---
+++ thingsCloud.lua
@@ -0,0 +1,226 @@
+-- 合宙模组 luat 接入 ThingsCloud 云平台的代码库
+-- ThingsCloud MQTT 接入文档：https://docs.thingscloud.xyz/guide/connect-device/mqtt.html
+
+require "log"
+require "http"
+require "mqtt"
+
+module(..., package.seeall)
+
+local projectKey = "" -- project_key
+local accessToken = "" -- access_token
+local host, port = "", 1883
+local mqttc = nil
+local connected = false
+
+local SUBSCRIBE_PREFIX = {
+    ATTRIBUTES_GET_REPONSE = "attributes/get/response/",
+    ATTRIBUTES_PUSH = "attributes/push",
+    COMMAND_SEND = "command/send/",
+    COMMAND_REPLY_RESPONSE = "command/reply/response/",
+    DATA_SET = "data/"
+}
+local EVENT_TYPES = {
+    connect = true,
+    attributes_report_response = true,
+    attributes_get_response = true,
+    attributes_push = true,
+    command_send = true,
+    command_reply_response = true,
+    data_set = true
+}
+local CALLBACK = {}
+local QUEUE = {
+    PUBLISH = {}
+}
+local logger = {}
+function logger.info(...)
+    log.info("ThingsCloud", ...)
+end
+
+function on(eType, cb)
+    if not eType or not EVENT_TYPES[eType] or type(cb) ~= "function" then
+        return
+    end
+    CALLBACK[eType] = cb
+    logger.info("on", eType)
+end
+
+local function cb(eType, ...)
+    if not eType or not EVENT_TYPES[eType] or not CALLBACK[eType] then
+        return
+    end
+    CALLBACK[eType](...)
+    logger.info("cb", eType, ...)
+end
+
+local function subscribe(topic)
+    logger.info("subscribe", topic)
+    mqttc:subscribe(topic)
+end
+
+local function publish(topic, data)
+    logger.info("publish", topic, data)
+    mqttc:publish(topic, data)
+end
+
+function isConnected()
+    return connected
+end
+
+local function mqttConnect()
+    local retryCount = 0
+    logger.info("thingscloud connecting...")
+    while not mqttc:connect(host, port) do
+        -- 重试连接
+        logger.info("mqtt reconnecting...")
+        sys.wait(5000)
+        retryCount = retryCount + 1
+        if (retryCount > 5) then
+            cb("connect", false)
+            return
+        end
+    end
+    connected = true
+    logger.info("thingscloud connected")
+
+    cb("connect", true)
+
+    subscribe("attributes/push")
+    subscribe("attributes/get/response/+")
+    subscribe("command/send/+")
+    subscribe("command/reply/response/+")
+end
+
+function connect(param)
+    host = param.host
+    projectKey = param.projectKey
+    accessToken = param.accessToken
+    sys.taskInit(
+        function()
+            while not socket.isReady() do
+                logger.info("wait socket ready...")
+                sys.wait(2000)
+            end
+            sys.taskInit(procConnect)
+        end
+    )
+end
+
+function procConnect()
+    mqttc = mqtt.client(misc.getImei(), 300, accessToken, projectKey)
+    mqttConnect()
+    if not isConnected() then
+        return
+    end
+    while true do
+        while #QUEUE.PUBLISH > 0 do
+            local item = table.remove(QUEUE.PUBLISH, 1)
+            if publish(item.topic, item.data) then
+            end
+        end
+
+        local result, data, param = mqttc:receive(100, "pub_msg")
+        if result then
+            logger.info("mqtt:receive", data.topic or nil, data.payload or "nil")
+            if
+                (data.topic:sub(1, SUBSCRIBE_PREFIX.ATTRIBUTES_GET_REPONSE:len()) ==
+                    SUBSCRIBE_PREFIX.ATTRIBUTES_GET_REPONSE)
+             then
+                local response = json.decode(data.payload)
+                local responseId = tonumber(data.topic:sub(SUBSCRIBE_PREFIX.ATTRIBUTES_GET_REPONSE:len() + 1))
+                cb("attributes_get_response", response, responseId)
+            elseif (data.topic == SUBSCRIBE_PREFIX.ATTRIBUTES_PUSH) then
+                local response = json.decode(data.payload)
+                cb("attributes_push", response)
+            elseif (data.topic:sub(1, SUBSCRIBE_PREFIX.COMMAND_SEND:len()) == SUBSCRIBE_PREFIX.COMMAND_SEND) then
+                local response = json.decode(data.payload)
+                if response.method and response.params then
+                    cb("command_send", response)
+                end
+            elseif
+                (data.topic:sub(1, SUBSCRIBE_PREFIX.COMMAND_REPLY_RESPONSE:len()) ==
+                    SUBSCRIBE_PREFIX.COMMAND_REPLY_RESPONSE)
+             then
+                local response = json.decode(data.payload)
+                local replyId = tonumber(data.topic:sub(SUBSCRIBE_PREFIX.COMMAND_REPLY_RESPONSE:len() + 1))
+                cb("command_reply_response", response, replyId)
+            elseif (data.topic:sub(1, SUBSCRIBE_PREFIX.DATA_SET:len()) == SUBSCRIBE_PREFIX.DATA_SET) then
+                local tmp = split(data.topic, "/")
+                if #tmp == 3 and tmp[3] == "set" then
+                    local identifier = tmp[2]
+                    cb("data_set", data.payload)
+                end
+            end
+        elseif data == "pub_msg" then
+        elseif data == "timeout" then
+        elseif data == "CLOSED" then
+            connected = false
+            logger.info("mqtt closed")
+            mqttc:disconnect()
+            sys.wait(3000)
+            mqttConnect()
+        else
+            connected = false
+            logger.info("mqtt disconnected")
+            mqttc:disconnect()
+            sys.wait(3000)
+            mqttConnect()
+        end
+    end
+end
+
+function reportAttributes(tableData)
+    table.insert(QUEUE.PUBLISH, {topic = "attributes", data = json.encode(tableData)})
+    sys.publish("QUEUE_PUBLISH", "ATTRIBUTES")
+end
+
+function getAttributes(attrsList, options)
+    options = options or {}
+    options.getId = options.getId or 1000
+    table.insert(
+        QUEUE.PUBLISH,
+        {
+            topic = "attributes/get/" .. tostring(options.getId),
+            data = json.encode(
+                {
+                    keys = attrsList
+                }
+            )
+        }
+    )
+end
+
+function reportEvent(event, options)
+    options = options or {}
+    options.eventId = options.eventId or 1000
+    table.insert(QUEUE.PUBLISH, {topic = "event/report/" .. tostring(options.eventId), data = json.encode(event)})
+end
+
+function replyCommand(commandReply, options)
+    options = options or {}
+    options.replyId = options.replyId or 1000
+    table.insert(
+        QUEUE.PUBLISH,
+        {topic = "command/reply/" .. tostring(options.replyId), data = json.encode(commandReply)}
+    )
+end
+
+function publishCustomTopic(identifier, data, options)
+    if type(identifier) ~= "string" then
+        return
+    end
+    table.insert(QUEUE.PUBLISH, {topic = "data/" .. identifier, data = data})
+end
+
+function split(str, sep)
+    local sep, fields = sep or ":", {}
+    local pattern = string.format("([^%s]+)", sep)
+    str:gsub(
+        pattern,
+        function(c)
+            fields[#fields + 1] = c
+        end
+    )
+    return fields
+end
